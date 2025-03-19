af695a4762b309c533e69035ff27264625a52708
SerAta26 <atamassergey26@gmail.com>
2025-03-19 22:52:53 +0300

    Update task_4.py

--- PART_1/task_4.py
+++ PART_1/task_4.py
@@ -1,11 +1,11 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
 
 unic_set = {
-    "Общее количество": 0,
-    "Уникальные посещения": 0
+    'Общее количество': 0,
+    'Уникальные посещения': 0
 }
 
-unic_set["Общее количество"] = len(users)
-unic_set["Уникальные посещения"] = len(set(users))
+unic_set['Общее количество='] = len(users)
+unic_set['Уникальные посещения'] = len(set(users))
 
 print(unic_set)
